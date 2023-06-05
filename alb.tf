85c9e7565794cd5a669af2fe07d8b8068b1fd46e
Praveen Gajulapalle <Praveen.gaju8994@gmail.com>
2023-06-05 11:08:54 +0530

    updating tf-module-alb directory

--- alb.tf
+++ alb.tf
@@ -1,13 +1,42 @@
-resource "aws_lb" "alb" {
+#Application load Balancer
+resource "aws_lb" "main" {
   name               = "${var.name}-${var.env}"
   internal           = var.internal
   load_balancer_type = var.load_balancer_type
   subnets            = var.subnets
+  security_groups    = [aws_security_group.main.id]
 
   enable_deletion_protection = var.enable_deletion_protection
 
   tags       = merge(
     var.tags,
     { Name = "${var.name}-${var.env}-alb" }
+  )
+}
+
+#security group
+resource "aws_security_group" "main" {
+  name        = "${var.name}-${var.env}"
+  description = "${var.name}-${var.env}"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description      = "APP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = var.allow_cidr
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags       = merge(
+    var.tags,
+    { Name = "${var.name}-${var.env}-lb" }
   )
 }
--- vars.tf
+++ vars.tf
@@ -8,3 +8,5 @@ variable "subnets" {}
 variable "enable_deletion_protection" {
   default = false
 }
+variable "vpc_id" {}
+variable "allow_cidr" {}
