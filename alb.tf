822c6999a94a6bfdbe7ead63b2bfdab75557ad83
Praveen Gajulapalle <Praveen.gaju8994@gmail.com>
2023-06-05 11:38:51 +0530

    updating tf-module-alb directory

--- alb.tf
+++ alb.tf
@@ -14,6 +14,24 @@ resource "aws_lb" "main" {
   )
 }
 
+#WS load Balancer fixed response
+resource "aws_lb_listener" "main" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/html"
+      message_body = "<h1>503 - Invalid Request</h1>"
+      status_code  = "503"
+    }
+  }
+}
+
+
 #security group
 resource "aws_security_group" "main" {
   name        = "${var.name}-${var.env}"
